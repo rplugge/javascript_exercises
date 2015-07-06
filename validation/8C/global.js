b6325f3642b6f8def9171a87801f1afa802345ed
Robert Plugge <robmplugge@hotmail.com>
2015-07-06 18:32:58 -0500

    Finished 8-C

---
+++ validation/8C/global.js
@@ -0,0 +1,7 @@
+var userNum = prompt("Give me a number between 1 and 8")
+var userNum = parseInt(userNum)
+
+for (;userNum <= 8 && userNum >= 1;) {
+  var userNum = prompt("Give me a number between 1 and 8")
+  var userNum = parseInt(userNum)
+}
