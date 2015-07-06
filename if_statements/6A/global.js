07fc5ded5c5bacb8da6bee6fc66aecfd83b263ff
Robert Plugge <robmplugge@hotmail.com>
2015-07-06 13:56:40 -0500

    Part 6-A finished

---
+++ if_statements/6A/global.js
@@ -0,0 +1,16 @@
+console.log("5 == '5' is " + (5 == '5') ); //true
+console.log("5 == 'five' is " + (5 == 'five') ); //false
+console.log("5 == '6' is " + (5 == '6') ); //false
+console.log("5 === '5' is " + (5 === '5') ); //false
+console.log("5 == 2 + 3 is " + (5 == 2+3) ); //true
+console.log("5 == 5 is " + (5 == 5) ); //true
+console.log("5 === 5 is " + (5 === 5) ); //true
+console.log("true == 'true' is " + (true == 'true') ); //true
+console.log("true === 'true' is " + (true === 'true') ); //false
+console.log("true == false is " + (true == false) ); //false
+console.log("5 == 12 is " + (5 == 12) ); //false
+console.log("5 !== 12 is " + (5 != 12) ); //true
+console.log("5 < 12 is " + (5 < 12) ); //true
+console.log("5 <= 12 is " + (5 <= 12) ); //true
+console.log("5 > 12 is " + (5 > 12) ); //false
+console.log("5 >= 12 is " + (5 >= 12) ); //false
