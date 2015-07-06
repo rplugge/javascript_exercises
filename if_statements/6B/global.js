8251c426652d8965d44c656fc49b12f07c007137
Robert Plugge <robmplugge@hotmail.com>
2015-07-06 14:05:29 -0500

    Finished 06-B

---
+++ if_statements/6B/global.js
@@ -0,0 +1,9 @@
+var password = "Blastech11"
+
+var user_password = prompt("What is the password?")
+
+if (user_password == password) {
+  alert("Welcome!")
+} else {
+  alert("Sorry, incorrect password.")
+}
