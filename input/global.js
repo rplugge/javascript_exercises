81fc7c70c2e40ddf23e0094d88d425515a5e30f5
Robert Plugge <robmplugge@hotmail.com>
2015-07-06 12:30:54 -0500

    Input Finished

---
+++ input/global.js
@@ -0,0 +1,16 @@
+var userName = prompt("Please Enter Your Name");
+var userAge = prompt("Please Enter Your Age");
+var userHometown = prompt("Please Enter Your Hometown");
+var userFavNumber = prompt("Please Enter Your Favorite Number");
+
+alert("Your name is " + userName);
+console.log("userName is " + userName);
+
+alert("Your age is " + userAge);
+console.log("userAge is " + userAge);
+
+alert("Your hometown is " + userHometown);
+console.log("userHometown is " + userHometown);
+
+alert("Your favorite number is " + userFavNumber);
+console.log("userFavNumber is " + userFavNumber);
