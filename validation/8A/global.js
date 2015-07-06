76b57c43c9516ec6cc35d370a5a53acf79b8f47e
Robert Plugge <robmplugge@hotmail.com>
2015-07-06 18:20:59 -0500

    Finished 8-A

---
+++ validation/8A/global.js
@@ -0,0 +1,8 @@
+
+var userPass = prompt("Please enter the password.")
+
+for (var password = "Blastech11"; userPass != password;) {
+  var userPass = prompt("Sorry, that was incorrect. Please enter the password.")
+}
+
+alert("Welcome!! :D")
