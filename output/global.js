02e8b8deee1102386d2e45cc81bd0c036a1fe86b
Robert Plugge <robmplugge@hotmail.com>
2015-07-06 11:51:39 -0500

    Output finished

---
+++ output/global.js
@@ -0,0 +1,9 @@
+alert ("Hello there. My name is Rob.");
+
+console.log("My favorite breakfast food is french toast.");
+console.log("Currently I live with 1 cat.");
+console.log("I used to eat lots of food at China One Buffet.");
+console.log("I take 0 naps per week.");
+console.log("I would like to take 7 naps a week.");
+
+alert ("Goodbye!")
